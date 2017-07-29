ba5dd8610ec2e3aec695c1c247da3cf167fa3594
krtuartier <krtuartier@gmail.com>
2017-07-30 01:10:16 +0800

    update

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -1,5 +1,4 @@
 window.onload=function(){
-	console.log(navigator);
 	txt = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
 	txt+= "<p>Browser Name: " + navigator.appName + "</p>";
 	txt+= "<p>Browser Version: " + navigator.appVersion + "</p>";
@@ -9,6 +8,10 @@ window.onload=function(){
 	txt+= "<p>User-agent language: " + navigator.systemLanguage + "</p>";
 	
 	document.getElementById("example").innerHTML=txt;
+	if(navigator.appVersion.indexOf('UCBrowser')>-1) {
+		document.body.style.backgroundColor='#000000';
+		document.body.style.color='#FFFFFF';
+	}
 	var oH1=document.querySelector('h1');
 	setInterval(function(){	
 		var array=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
