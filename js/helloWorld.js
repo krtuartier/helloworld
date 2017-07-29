e82e6216ab6b0fb486c45e597eebdd252979baaa
krtuartier <krtuartier@gmail.com>
2017-07-30 00:41:35 +0800

    update

---
+++ js/helloWorld.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+	console.log(navigator);
+	txt = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+	txt+= "<p>Browser Name: " + navigator.appName + "</p>";
+	txt+= "<p>Browser Version: " + navigator.appVersion + "</p>";
+	txt+= "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+	txt+= "<p>Platform: " + navigator.platform + "</p>";
+	txt+= "<p>User-agent header: " + navigator.userAgent + "</p>";
+	txt+= "<p>User-agent language: " + navigator.systemLanguage + "</p>";
+	
+	document.getElementById("example").innerHTML=txt;
+	var oH1=document.querySelector('h1');
+	setInterval(function(){	
+		var array=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+		oH1.style.color='#'+
+		array[parseInt(Math.random()*16)]+
+		array[parseInt(Math.random()*16)]+
+		array[parseInt(Math.random()*16)]+
+		array[parseInt(Math.random()*16)]+
+		array[parseInt(Math.random()*16)]+
+		array[parseInt(Math.random()*16)];
+	},200);
+}
