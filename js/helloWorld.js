dc48a7ddda5128de76a976b042095d66f89faf7f
krtuartier <krtuartier@gmail.com>
2017-08-01 20:16:33 +0800

    updata

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -1,4 +1,17 @@
 window.onload=function(){
+	var txt = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+	txt+= "<p>Browser Name: " + navigator.appName + "</p>";
+	txt+= "<p>Browser Version: " + navigator.appVersion + "</p>";
+	txt+= "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+	txt+= "<p>Platform: " + navigator.platform + "</p>";
+	txt+= "<p>User-agent header: " + navigator.userAgent + "</p>";
+	txt+= "<p>User-agent language: " + navigator.systemLanguage + "</p>";
+	document.getElementById("example").innerHTML=txt;
+	var oUcb=navigator.appVersion+'';
+	if(oUcb.indexOf('UCBrowser')>-1) {
+		document.body.style.backgroundColor='#000000';
+		document.body.style.color='#FFFFFF';
+	}
 	var oH1=document.querySelector('h1');
 	setInterval(function(){	
 		var array=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
@@ -9,6 +22,5 @@ window.onload=function(){
 		array[parseInt(Math.random()*16)]+
 		array[parseInt(Math.random()*16)]+
 		array[parseInt(Math.random()*16)];
-		console.log(parseInt(Math.random()*16));
 	},200);
 }
